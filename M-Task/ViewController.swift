3a03930c48ebb33af8d7c13b3719eec4a728f661
Kaustubh Gharat <gharat.kaustubh@gmail.com>
2023-01-06 21:08:49 +0530

    Initial Commit

---
+++ M-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M-Task
+//
+//  Created by Kaustubh Gharat on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
